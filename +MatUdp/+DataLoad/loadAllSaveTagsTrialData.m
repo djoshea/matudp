d59669d2bfd3fabafefc9b98fc2cd2c7f630fbc9
Dan O'Shea <dan@djoshea.com>
2017-07-17 18:34:32 -0700

    Interactive save tag loading

--- +MatUdp/+DataLoad/loadAllSaveTagsTrialData.m
+++ +MatUdp/+DataLoad/loadAllSaveTagsTrialData.m
@@ -1,5 +1,8 @@
 function [td, saveTags] = loadAllSaveTagsTrialData(folder)
 
+if nargin < 1
+    folder = '';
+end
 [R, meta, saveTags] = MatUdp.DataLoad.loadAllSaveTagsRaw(folder);
 
 %debug('Building TrialData...\n');
